758ad7c11306efc17980b1c1a44e4f8e504c5c17
Trent Summerlin <whirvis@ardenus.org>
2021-07-14 16:15:01 -0500

    Add barebones boot and config

--- src/main/java/net/whirvis/mc/discraft/DiscraftPlugin.java
+++ src/main/java/net/whirvis/mc/discraft/DiscraftPlugin.java
@@ -1,7 +1,35 @@
 package net.whirvis.mc.discraft;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiscraftPlugin extends JavaPlugin {
-	
+
+	private final Logger log;
+	private final DiscraftConfig config;
+	private boolean enabled;
+
+	public DiscraftPlugin() {
+		this.log = this.getLogger();
+		this.config = new DiscraftConfig(this);
+	}
+
+	@Override
+	public void onEnable() {
+		config.load();
+		if (!config.hasBotToken()) {
+			log.severe("Failed to fetch bot token!");
+			return;
+		}
+		this.enabled = true;
+	}
+
+	@Override
+	public void onDisable() {
+		if (!enabled) {
+			return;
+		}
+	}
+
 }
